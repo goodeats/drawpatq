ed317abe57f5f14e32b40547ed2a98069acc5dc1
Pat <patrick.h.needham@gmail.com>
2020-04-16 22:28:29 -0700

    added Canvas class to strip functionality out of App for reload prep

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
-import Triangle from './components/Triangle';
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-header-h1">10,000 Triangles</h1>
-        {Array.from(Array(10000), (e, i) => {
-          return (
-            <Triangle
-              key={i}
-              lowerWidth={10}
-              upperWidth={20}
-            />
-          );
-        })}
       </header>
+      <Canvas />
     </div>
   );
 }
---
+++ src/components/Canvas.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Triangle from "./shapes/Triangle";
+
+export default class Canvas extends React.Component {
+
+  render() {
+    return (
+      <main>
+        {Array.from(Array(10000), (e, i) => {
+          return <Triangle key={i} lowerWidth={10} upperWidth={20} />;
+        })}
+      </main>
+    );
+  }
+}
